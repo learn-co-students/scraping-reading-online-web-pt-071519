8c93576c2281795d8b35b3173f57e6d83015e48a
assane savadogo <assane.savadogo81@gmail.com>
2019-08-17 18:08:27 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,6 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
-p doc.css(".tout__label.heading.heading--level-4")[0]
+p doc.css(".tout__label.heading.heading--level-4").text
 
 
