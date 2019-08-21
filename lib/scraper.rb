ddb4cd8a7ef8e1d06d20c74b0333a149ad7dced0
Annie Truex <artruex@gmail.com>
2019-08-21 22:59:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,11 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+doc = Nokigiri::HTML(open("http://flatironschool.com/"))
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+  puts course.text.strip
+end
 
 
