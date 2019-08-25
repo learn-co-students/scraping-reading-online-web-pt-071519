6de2cff8feaae11bccde99f557c8bdbc96c8867d
Thomas DiGregorio <digregoriothomas@yahoo.com>
2019-08-24 21:02:01 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,10 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(html)
+doc = Nokogiri::(open("https://flatironschool.com/"))
+doc.css(".site-header_hero_headline").text
+
+
 
 
