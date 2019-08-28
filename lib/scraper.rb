06385c90540250eb6515b80be06b4457844be40b
Alouiscious Media <info@reneesoul.com>
2019-08-28 02:20:14 -0700

    Update scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("http://www.suzesims.com/")
+html = open("https://www.suzesims.com/")
 
+# take the string of HTML returned
+# by open-uri's `open` method and convert it into a NodeSet 
 
-
+doc = Nokogiri::HTML(html)
+puts doc
