3ac5e21b91a43217d490a2a571e825d16bbc4696
Patrick Harrison <jahman@bellatlantic.net>
2019-08-29 01:16:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
 
+
+
 html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(html)
+doc.css(".site-header__hero__headline")
 
-
+require 'nokogiri'
+require 'open-uri'
+ 
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".tout__label.heading.heading--level-4")
